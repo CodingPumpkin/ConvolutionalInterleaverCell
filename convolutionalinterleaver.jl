6aa3e713c18e5d748af57ba7882ba55879ad861a
Daniel <questytheswordsman@gmail.com>
2024-11-17 22:43:18 +0300

    Added types to arguments and function, added initial conditions

--- convolutionalinterleaver.jl
+++ convolutionalinterleaver.jl
@@ -1,23 +1,21 @@
 using Plots
 x = range(0, 1, length=400)
-#x = range(0, 2π, length=400)
 
-#y = sin.(x)
 y = @. sin(x) + 0.001 * randn()
 plot(x,y)
 
-function convolutionalinterleaver_shiftregisters(inputdata::Vector, number::Int, size::Int)
-	outputdata = zeros(length(inputdata))
-	shiftregisters = zeros(number, size)
+function convolutionalinterleaver_shiftregisters(inputdata::Vector{Q}, number::Int, size::Int, initialconditions::Q) :: Vector{Q} where Q
+	outputdata = Vector{typeof(initialconditions)}(undef, length(inputdata))
+	shiftregisters = fill(initialconditions, (13, 7))
 	readindex = 1
 	writeindex = 1
-	while readindex+size <= length(inputdata)
+	while readindex+size < length(inputdata)
 		for j in 1:number
 			shiftregisters[j, 1:size] = reverse(inputdata[readindex:(readindex+size-1)])
 			readindex += size			
 			if readindex >= length(inputdata)
 				break
-			end
+			end  
 		end
 		for j in 1:number
 			outputdata[writeindex:writeindex+size-1] = shiftregisters[j, 1:size]
@@ -27,9 +25,8 @@ function convolutionalinterleaver_shiftregisters(inputdata::Vector, number::Int,
 			end
 		end
 	end
-	outputdata[writeindex:length(inputdata)] =inputdata[writeindex:length(inputdata)]
+	outputdata[writeindex:length(inputdata)] = inputdata[writeindex:length(inputdata)]
 	return outputdata
 end
 
 plot(x, [y, convolutionalinterleaver_shiftregisters(y, 13, 7)])
-#plot(x, convolutionalinterleaver_shiftregisters(y, 13, 7))
