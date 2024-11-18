af88a752e1869933d02335cf3001ecd432cb480f
Daniel <questytheswordsman@gmail.com>
2024-11-18 14:11:51 +0300

    Added an alternative function for convolutional interleaving that uses new shift registers

--- convolutionalinterleaver.jl
+++ convolutionalinterleaver.jl
@@ -44,4 +44,40 @@ function initshiftregisters(maxregistersize::Int, numberofregisters::Int, regist
 	return registers
 end
 
+function convinterleaver_fancyshiftregisters(y::Vector{Q}, numberofregisters::Int, registerlengthstep::Int, initialconditions::Q)::Vector{Q} where Q
+	framesize = length(y)
+	maxsize = 8 #I decided to use this value as a default one
+	if registerlengthstep > 0
+		maxsize = (numberofregisters-1)*registerlengthstep	
+	end
+	outputdata = fill(initialconditions, framesize)
+	shiftregisters = initshiftregisters(maxsize, numberofregisters, registerlengthstep, initialconditions)
+	readindex = 1
+	writeindex = 1
+	while readindex < framesize
+		for n in 1:numberofregisters
+			for k in 1:length(shiftregisters[n])
+				if readindex == framesize+1
+					break
+				end
+				shiftregisters[n][k] = y[readindex]
+				readindex += 1
+			end
+		end
+		for n in 1:numberofregisters
+			for k in 1:length(shiftregisters[n])
+				if writeindex == framesize+1
+					break
+				end
+				outputdata[writeindex] = shiftregisters[n][length(shiftregisters[n])+1-k]
+				writeindex += 1
+			end
+		end
+ 	end
+	println(outputdata)
+	return outputdata
+end
+
 plot(x, [y, convolutionalinterleaver_shiftregisters(y, 13, 7)])
+
+plot(x, [y, convinterleaver_fancyshiftregisters(y, 6, 0, 0.0)])
